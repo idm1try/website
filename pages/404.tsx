86bd7798e58e2273e49685a488e5bc7bb8050fb9
idm1try <dimasavchenko1706@icloud.com>
2022-10-09 12:55:35 +0500

    feat: update 404 page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,21 +1,23 @@
-import { Box, Button, Heading, Text } from '@chakra-ui/react';
+import { Box, Button, Center, Text } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { TbHome } from 'react-icons/tb';
 import Layout from 'layouts/Article';
+import ChakraNextImage from 'components/ChakraNextImage';
 
 const NotFound = () => {
   return (
     <Layout title='404: Not found' description='Page not found'>
-      {' '}
       <Box textAlign='center' mt={5}>
-        <Heading display='inline-block' as='h2' size='2xl' color='accent'>
-          404
-        </Heading>
-        <Text fontSize='18px' mt={3} mb={2}>
-          Page Not Found
-        </Text>
-        <Text color='gray.500' mb={6}>
-          The page you&apos;re looking for does not seem to exist
+        <ChakraNextImage
+          as={Center}
+          src='/travolta.webp'
+          height='500'
+          width='500'
+          alt='Travola Lost'
+          filter='brightness(1.25) contrast(1.25) grayscale(100%)'
+        />
+        <Text color='gray.500' my={6}>
+          Whoops, it looks like the page you were looking for doesn&apos;t exist!
         </Text>
         <NextLink href='/'>
           <Button colorScheme='teal' leftIcon={<TbHome />}>
