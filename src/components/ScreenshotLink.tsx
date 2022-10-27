05a3a9a375739229bc256294d73f2daa3226e839
idm1try <dimasavchenko1706@icloud.com>
2022-10-27 20:22:23 +0500

    chore: improve screenshot link

--- src/components/ScreenshotLink.tsx
+++ src/components/ScreenshotLink.tsx
@@ -1,6 +1,6 @@
 import { AbsoluteCenter, Box, Link, Spinner, useColorModeValue } from '@chakra-ui/react';
-import { ReactNode, useState } from 'react';
 import { motion } from 'framer-motion';
+import { ReactNode, useState } from 'react';
 import Image from './Image';
 
 export interface ScreenshotLinkProps {
@@ -10,6 +10,7 @@ export interface ScreenshotLinkProps {
 
 const ScreenshotLink = ({ href, children }: ScreenshotLinkProps) => {
   const [isHovering, setIsHovering] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [linkScreenshot, setLinkScreenshot] = useState('');
   const bgColor = useColorModeValue('gray.50', 'gray.900');
 
@@ -18,7 +19,8 @@ const ScreenshotLink = ({ href, children }: ScreenshotLinkProps) => {
       setIsHovering(true);
       const res = await fetch(`/api/screenshot-link?url=${encodeURIComponent(url)}`);
       const image = await res.blob();
-      setLinkScreenshot(URL.createObjectURL(image));
+      setLinkScreenshot(res.status !== 500 && URL.createObjectURL(image));
+      setIsLoading(false);
     } catch (e) {
       console.error('Error fetching screenshot image', e);
     }
@@ -34,56 +36,68 @@ const ScreenshotLink = ({ href, children }: ScreenshotLinkProps) => {
         onBlur={() => setIsHovering(false)}
       >
         {isHovering && (
-          <Box>
-            {linkScreenshot ? (
-              <motion.div
-                initial={{ opacity: 0, x: 0, y: '3.2rem' }}
-                animate={{ opacity: 1, x: 0, y: '2.0rem' }}
-                transition={{ duration: 0.4, ease: 'easeInOut' }}
-                style={{ position: 'absolute' }}
+          <motion.div
+            initial={{ opacity: 0, x: 0, y: '3.2rem' }}
+            animate={{ opacity: 1, x: 0, y: '2.0rem' }}
+            transition={{ duration: 0.4, ease: 'easeInOut' }}
+            style={{ position: 'absolute' }}
+          >
+            {isLoading ? (
+              <Box
+                pos='absolute'
+                pointerEvents='none'
+                display='block'
+                zIndex={9}
+                bgColor={bgColor}
+                height={124}
+                width={52}
+                shadow='lg'
+                bottom='2.0rem'
+                rounded='lg'
               >
-                <Image
-                  src={linkScreenshot}
-                  height={180}
-                  width={300}
-                  alt='Link preview'
-                  rounded='lg'
-                  pointerEvents='none'
-                  pos='absolute'
-                  bottom='2.0rem'
-                  zIndex={9}
-                  display='block'
-                  h={124}
-                  w={52}
-                  shadow='lg'
-                />
-              </motion.div>
+                <AbsoluteCenter>
+                  <Spinner color='accent' size='lg' thickness='4px' />
+                </AbsoluteCenter>
+              </Box>
             ) : (
-              <motion.div
-                initial={{ opacity: 0, x: 0, y: '3.2rem' }}
-                animate={{ opacity: 1, x: 0, y: '2.0rem' }}
-                transition={{ duration: 0.4, ease: 'easeInOut' }}
-                style={{ position: 'absolute' }}
-              >
-                <Box
-                  pos='absolute'
-                  pointerEvents='none'
-                  display='block'
-                  zIndex={9}
-                  bgColor={bgColor}
-                  height={124}
-                  width={52}
-                  shadow='lg'
-                  bottom='2.0rem'
-                  rounded='lg'
-                >
-                  <AbsoluteCenter>
-                    <Spinner color='accent' size='lg' thickness='4px' />
-                  </AbsoluteCenter>
-                </Box>
-              </motion.div>
+              <Box>
+                {linkScreenshot ? (
+                  <Image
+                    src={linkScreenshot}
+                    height={180}
+                    width={300}
+                    alt='Link preview'
+                    rounded='lg'
+                    pointerEvents='none'
+                    pos='absolute'
+                    bottom='2.0rem'
+                    zIndex={9}
+                    display='block'
+                    h={124}
+                    w={52}
+                    shadow='lg'
+                  />
+                ) : (
+                  <Box
+                    pos='absolute'
+                    pointerEvents='none'
+                    display='block'
+                    zIndex={9}
+                    bgColor={bgColor}
+                    height={124}
+                    width={52}
+                    shadow='lg'
+                    bottom='2.0rem'
+                    rounded='lg'
+                  >
+                    <AbsoluteCenter color='accent' fontWeight='bold'>
+                      Error or not found
+                    </AbsoluteCenter>
+                  </Box>
+                )}
+              </Box>
             )}
-          </Box>
+          </motion.div>
         )}
         <Link href={href}>{children}</Link>
       </Box>
