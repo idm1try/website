fcf3f815968f4a6d110e99207e902cb2d6c10ff1
idm1try <dimasavchenko1706@icloud.com>
2022-11-10 16:56:23 +0500

    chore(deps): update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { Box, Icon, Stack, StackProps, Text, VStack } from '@chakra-ui/react';
+import { Icon, Stack, StackProps, Text, VStack } from '@chakra-ui/react';
 import { TbBrandGithub, TbBrandTwitter, TbHeart, TbMail, TbWorld } from 'react-icons/tb';
 import SocialLink from './SocialLink';
 
@@ -14,20 +14,6 @@ export const Footer = (props: StackProps) => (
       <SocialLink icon={TbBrandTwitter} href='https://twitter.com/idm1try' label='Twitter' />
       <SocialLink icon={TbMail} href='mailto:admin@idm1try.ru' label='Mail' />
     </Stack>
-
-    <Box
-      as='a'
-      href='https://nesteroff561.ru'
-      fontSize='sm'
-      fontWeight='semibold'
-      bg='black'
-      color='white'
-      px='4'
-      py='2'
-      rounded='lg'
-    >
-      Deployed by nesteroff561
-    </Box>
   </VStack>
 );
 
