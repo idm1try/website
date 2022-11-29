49cd6cee61b4d9ebcc1d071a26931594e13ea2f2
idm1try <dimasavchenko1706@icloud.com>
2022-11-29 23:25:35 +0500

    feat(404): update

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,20 +1,28 @@
 import Layout from '@/components/Layout';
+import Head from 'next/head';
 import Link from 'next/link';
 import { TbHome } from 'react-icons/tb';
 
 const NotFound = () => (
-  <Layout className='text-center'>
-    <div className='my-10'>
-      <h1 className='animate-fade_in_up_10 text-5xl font-bold text-pink-200 dark:text-pink-100'>
+  <Layout>
+    <Head>
+      <title>Page Not Found | idm1try</title>
+    </Head>
+    <div className='relative z-10 my-16 flex max-w-7xl flex-col items-start text-left lg:my-[20vh] lg:flex-row lg:items-center lg:justify-between'>
+      <div className='mb-8 animate-fade_in_up_10 pl-2 text-6xl font-bold tracking-tight text-pink-200 dark:text-pink-100 lg:pl-0 lg:pr-8'>
         404
-      </h1>
-      <p className='my-5 text-xl font-medium'>Oops, not found</p>
+      </div>
+      <div className='relative w-full p-2 text-left leading-8 lg:w-11/12 lg:pl-10'>
+        <div className='text-3xl text-subtext0-200 dark:text-subtext0-100'>
+          Whoops, it looks like the page you were looking for doesn&apos;t exist!
+        </div>
+        <Link href='/' legacyBehavior>
+          <button className='mt-8 select-none rounded-lg bg-mantle-200 py-2 px-4 font-bold text-pink-200 transition-colors duration-300 hover:bg-surface0-200 active:bg-surface1-200 dark:bg-mantle-100 dark:text-pink-100 dark:hover:bg-surface0-100 dark:active:bg-surface1-100'>
+            <TbHome className='mb-1 inline-block' /> Go to home
+          </button>
+        </Link>
+      </div>
     </div>
-    <Link href='/'>
-      <button className='mt-3 select-none rounded-lg bg-pink-200 py-2 px-4 font-bold text-base-200 transition-colors duration-500 hover:bg-flamingo-200 active:bg-rosewater-200 dark:bg-pink-100 dark:text-base-100 dark:hover:bg-flamingo-100 dark:active:bg-rosewater-100'>
-        <TbHome className='mb-1 inline-block' /> Go to home
-      </button>
-    </Link>
   </Layout>
 );
 
