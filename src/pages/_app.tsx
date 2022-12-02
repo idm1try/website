cdc5197b5f651a69118790e2f3840a85b187de60
idm1try <dimasavchenko1706@icloud.com>
2022-12-03 00:27:42 +0500

    feat: inter font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,16 @@
 import '@/styles/index.css';
 import { AppProps } from 'next/app';
+import { Inter } from '@next/font/google';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+});
 
 const Website = ({ Component, pageProps, router }: AppProps) => (
-  <Component {...pageProps} key={router.route} />
+  <div className={`${inter.variable} font-sans`}>
+    <Component {...pageProps} key={router.route} />
+  </div>
 );
 
 export default Website;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
-/** @type {import('tailwindcss').Config} */
+/* eslint-disable-next-line @typescript-eslint/no-var-requires */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
+/* @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{ts,tsx,mdx,mdx}'],
   darkMode: 'class',
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['var(--font-inter)', ...fontFamily.sans],
+      },
       colors: {
         rosewater: {
           100: '#f5e0dc',
