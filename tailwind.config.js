7367d6e48f1efd42e6d1e65b2204e7d102108e8d
idm1try <dimasavchenko1706@icloud.com>
2022-12-23 15:44:46 +0500

    chore: cleanup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,9 @@
-/* eslint-disable-next-line @typescript-eslint/no-var-requires */
-const { fontFamily } = require('tailwindcss/defaultTheme')
-
 /* @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{ts,tsx,mdx,mdx}'],
   darkMode: 'class',
   theme: {
     extend: {
-      fontFamily: {
-        sans: ['Inter', ...fontFamily.sans],
-      },
       typography: theme => ({
         DEFAULT: {
           css: {
@@ -34,76 +28,11 @@ module.exports = {
       }),
       animation: {
         in: 'in .6s both',
-        fade_in: 'fade_in 1000ms forwards',
-        fade_in_up:
-          'fade_in_up 550ms cubic-bezier(0.68, -0.6, 0.32, 1.6) forwards',
-        fade_in_up_10:
-          'fade_in_up_10 750ms cubic-bezier(0.68, -0.6, 0.32, 2.2) forwards',
-        fade_in_up_5:
-          'fade_in_up_5 750ms cubic-bezier(0.68, -0.6, 0.32, 1.6) forwards',
-        fade_in_up_down:
-          'fade_in_up_down 1400ms cubic-bezier(0.68, -0.6, 0.32, 1.6) forwards',
       },
       keyframes: {
         in: {
           '0%': { transform: 'translateY(18px)', opacity: 0 },
           '100%': { transform: 'translateY(0)', opacity: 1 },
-        },
-        fade_in: {
-          '0%': {
-            opacity: '0',
-          },
-          '100%': {
-            opacity: '1',
-          },
-        },
-        fade_in_up: {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(20px)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
-          },
-        },
-        fade_in_up_10: {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(10px)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
-          },
-        },
-        fade_in_up_5: {
-          '0%': {
-            opacity: '0',
-            transform: 'translateX(50%) translateY(20px)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateX(50%) translateY(0)',
-          },
-        },
-        fade_in_up_down: {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(20px)',
-          },
-          '50%': {
-            opacity: '1',
-            transform: 'translateY(0)',
-          },
-          '70%': {
-            opacity: '1',
-            transform: 'translateY(0px)',
-          },
-          '100%': {
-            opacity: '0',
-            transform: 'translateY(10px)',
-          },
         },
       },
     },
