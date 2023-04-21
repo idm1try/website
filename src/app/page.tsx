dffcb605f7c49e4f7a9041c356f313632461a642
idm1try <dimasavchenko1706@icloud.com>
2023-04-21 23:24:45 +0500

    update email

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -25,7 +25,7 @@ const Home = () => (
         </li>
         <li>
           <MailIcon />
-          <a className='underlined' href='mailto:me@idm1try.ru'>me@idm1try.ru</a>
+          <a className='underlined' href='mailto:idm1try@icloud.com'>me@idm1try.ru</a>
         </li>
       </ul>
 
