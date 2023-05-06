4b28ed691c64b04af1c33e5dc422d1f98cd726a3
idm1try <dimasavchenko1706@icloud.com>
2023-05-06 22:31:13 +0500

    fix: deprecated

--- src/app/api/og/route.tsx
+++ src/app/api/og/route.tsx
@@ -1,8 +1,6 @@
 import { ImageResponse } from '@vercel/og'
 
-export const config = {
-  runtime: 'edge',
-}
+export const runtime = 'edge'
 
 const interRegular = fetch(
   new URL('../../../../public/fonts/Inter-Regular.ttf', import.meta.url),
